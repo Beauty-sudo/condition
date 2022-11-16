4f1cbc3bf31d2cdfd5cf689ee4ea63ad554836eb
opeyemi <operaomilude111@gmail.com>
2022-11-16 04:03:20 +0100

    condition

---
+++ condition-assignment.js
@@ -0,0 +1,17 @@
+let firstname = "opeyemi"
+if (firstname == 'opeyemi')
+document.getElementById('sup').innerHTML= "opeyemi is my first name"
+
+else if (firstname == "oluwakemi")
+document.getElementById('edu').innerHTML= "oluwakemi is my second name"
+
+else if (firstname == "okikiola")
+document.getElementById('get').innerHTML= "okikiola is my thrid name"
+
+else if (firstname == "beauty")
+document.getElementById('take').innerHTML= "beauty is my fourth name"
+
+else {
+    console.log("invalid")
+    }
+
